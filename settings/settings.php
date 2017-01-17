525d1a17e847688456ac043b32b798a1a75e3b41
Mark Pavlitski <markp@deeson.co.uk>
2017-01-17 20:55:50 +0000

    Added VDD settings.

--- settings/settings.php
+++ settings/settings.php
@@ -15,6 +15,14 @@ $base_domains = [
   'live-site.com' => 'live',
 ];
 
+// Environment settings for VDD.
+$local_settings = '/var/www/settings/PROJECT/settings.inc';
+if (file_exists($local_settings)) {
+  require_once $local_settings;
+  define('SETTINGS_ENVIRONMENT', 'local');
+  define('SETTINGS_HOSTING', 'vdd');
+}
+
 foreach (glob(dirname(DRUPAL_ROOT) . '/settings/*.settings.inc') as $file) {
   require_once $file;
 }
