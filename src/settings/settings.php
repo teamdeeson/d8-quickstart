c6e99591e9f1697d7cbbbd6bb7147980ece69856
Mark Pavlitski <markp@deeson.co.uk>
2017-03-05 11:18:36 +0000

    Moved environment detection out of settings.php

--- src/settings/settings.php
+++ src/settings/settings.php
@@ -3,34 +3,14 @@
 /**
  * @file
  * Default Drupal configuration file sites/default/settings.php.
+ *
+ * You should not normally need to modify this file.
  */
 
-/**
- * Map domains to environment types here.
- */
-$base_domains = [
-  'your-project-name.dev' => 'local',
-  'dev-site.com' => 'dev',
-  'test-site.com' => 'test',
-  'live-site.com' => 'live',
-];
+// Detect the environment.
+require_once dirname(DRUPAL_ROOT) . '/src/settings/environment.inc';
 
-// Environment settings for VDD.
-$local_settings = '/var/www/settings/PROJECT/settings.inc';
-if (file_exists($local_settings)) {
-  require_once $local_settings;
-}
-
-// Assume local development environment if no environment has been defined.
-if (!defined('SETTINGS_ENVIRONMENT')) {
-  define('SETTINGS_ENVIRONMENT', 'local');
-}
-
-// Assume the site is running on vdd if no hosting settings have been defined.
-if (!defined('SETTINGS_HOSTING')) {
-  define('SETTINGS_HOSTING', 'vdd');
-}
-
+// Configure the application.
 foreach (glob(dirname(DRUPAL_ROOT) . '/src/settings/*.settings.inc') as $file) {
   require_once $file;
 }
