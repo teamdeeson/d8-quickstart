7f245534ff7b75e29414e958a673ba510c8d69d4
Chris Jansen <chrisj@deeson.co.uk>
2017-06-02 08:06:35 +0100

    Add demo test.

---
+++ src/modules/unit_test_demo/tests/src/Unit/DemoTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\Tests\unit_test_demo\Unit;
+
+class DemoTest extends \PHPUnit_Framework_TestCase {
+  /**
+   * @test
+   */
+  public function thatWeHaveAFunctioningUnitTestEnvironment() {
+    self::assertTrue(TRUE);
+  }
+}
