38887cc859d885e7ec64bbaec578772f63e39d13
Chris Jansen <chrisj@deeson.co.uk>
2017-06-02 09:22:44 +0100

    Fix DemoTest coding standards.

--- src/modules/unit_test_demo/tests/src/Unit/DemoTest.php
+++ src/modules/unit_test_demo/tests/src/Unit/DemoTest.php
@@ -2,11 +2,16 @@
 
 namespace Drupal\Tests\unit_test_demo\Unit;
 
+/**
+ * @group demo
+ */
 class DemoTest extends \PHPUnit_Framework_TestCase {
+
   /**
    * @test
    */
-  public function thatWeHaveAFunctioningUnitTestEnvironment() {
+  public function thatTestEnvironmentIsFunctional() {
     self::assertTrue(TRUE);
   }
+
 }
