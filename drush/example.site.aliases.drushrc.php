4169e376bec5cec7b1bffc91b898c46c68c0b9a3
Mark Pavlitski <markp@deeson.co.uk>
2017-06-07 18:57:37 +0100

    Use docker-ssh connection to the PHP server instead of launcher script.

--- drush/example.site.aliases.drushrc.php
+++ drush/example.site.aliases.drushrc.php
@@ -26,6 +26,22 @@ if (!file_exists('/var/www/vhosts/PROJECT.dev/docroot')) {
 }
 
 $aliases['docker'] = array(
-  'php' => dirname(dirname(__FILE__)) . '/scripts/docker-drush-launcher.sh',
+  'env' => 'docker',
+  'uri' => 'localhost',
   'root' => '/var/www/html/docroot',
+  // Drush ignores the remote-host and does not attempt an SSH connection if
+  // the value is localhost or 127.0.0.1.
+  // This is incorrect.
+  // We can work around this by using any DNS entry which resolves to
+  // 127.0.0.1 such as localtest.me.
+  // You should add an entry to your hosts file such as the following, and
+  // replace the localtest.me hostname:
+  // 127.0.0.1 docker.local
+  // 'remote-host' => 'docker.local',
+  'remote-host' => 'localtest.me',
+  'remote-user' => 'www-data',
+  'ssh-options' => '-p 2223',
+  'path-aliases' => array(
+    '%drush-script' => '/var/www/html/vendor/bin/drush',
+  ),
 );
