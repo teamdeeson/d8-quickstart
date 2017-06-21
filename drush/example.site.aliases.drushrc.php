bc10001d7692be481279ef40f7cb24b68ded07e7
John Ennew <johne@deeson.co.uk>
2017-06-21 01:06:34 +0100

    Fixed the drush aliases for docker so it does not try and connect remotely when on the docker image

--- drush/example.site.aliases.drushrc.php
+++ drush/example.site.aliases.drushrc.php
@@ -29,19 +29,15 @@ $aliases['docker'] = array(
   'env' => 'docker',
   'uri' => 'localhost',
   'root' => '/var/www/html/docroot',
-  // Drush ignores the remote-host and does not attempt an SSH connection if
-  // the value is localhost or 127.0.0.1.
-  // This is incorrect.
-  // We can work around this by using any DNS entry which resolves to
-  // 127.0.0.1 such as localtest.me.
-  // You should add an entry to your hosts file such as the following, and
-  // replace the localtest.me hostname:
-  // 127.0.0.1 docker.local
-  // 'remote-host' => 'docker.local',
-  'remote-host' => 'localtest.me',
-  'remote-user' => 'www-data',
-  'ssh-options' => '-p 2223',
   'path-aliases' => array(
     '%drush-script' => '/var/www/html/vendor/bin/drush',
   ),
 );
+
+if (!file_exists('/var/www/html')) {
+  // You will need to add an entry to your hosts file as follows:
+  // 127.0.0.1 docker.local
+  $alias['docker']['remote-host'] = 'docker.local';
+  $alias['docker']['remote-user'] = 'www-data';
+  $alias['docker']['ssh-options'] = '-p 2223';
+}
