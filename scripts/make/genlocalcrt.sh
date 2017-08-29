34efa1f3a9b3c6c9ba9ab8f66cca29cb0366408f
Mark Pavlitski <markp@deeson.co.uk>
2017-08-29 15:33:20 +0100

    Add SAN records (docker.local and localhost) to the generated certificate.

--- scripts/make/genlocalcrt.sh
+++ scripts/make/genlocalcrt.sh
@@ -2,6 +2,8 @@
 
 DIR=${1-.}
 OPENSSL=`which openssl`
+OPENSSLDIR=`${OPENSSL} version -a | grep OPENSSLDIR |  awk '{gsub(/"/, "",  $2); print $2}'`
+OPENSSLCONF="${OPENSSLDIR:-/System/Library/OpenSSL}/openssl.cnf"
 
 if [ -f ${DIR}/local.key ] && [ -f ${DIR}/local.crt ]; then
     echo 'Certificate exists';
@@ -12,6 +14,10 @@ $OPENSSL rsa -passin pass:x -in ${DIR}/local.pass.key -out ${DIR}/local.key
 rm ${DIR}/local.pass.key
 $OPENSSL req -new -x509 -nodes -sha1 -days 3650 \
     -key ${DIR}/local.key -out ${DIR}/local.crt \
-    -subj "/C=GB/ST=Local/L=Local/O=Local/CN=localhost"
+    -subj "/C=GB/ST=Local/L=Local/O=Local/CN=localhost" \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat ${OPENSSLCONF} \
+        <(printf '[SAN]\nsubjectAltName=DNS:localhost,DNS:docker.local'))
 
 
