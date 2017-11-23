1c1868e473dfee51b6e2cac3609256e1da1a1783
Mark Pavlitski <markp@deeson.co.uk>
2017-11-23 11:13:34 +0000

    Pipelines build setup is no longer required.

--- scripts/make/fe.sh
+++ scripts/make/fe.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-# Install frontend build bits on bitbucket pipelines.
-if [ $(command -v apk) ]; then
-    apk update
-    apk add build-base gcc abuild binutils libpng-dev autoconf automake build-base libtool nasm
-    npm -g install yarn
-fi;
-
 # Build the frontend.
 cd src/frontend
 yarn
