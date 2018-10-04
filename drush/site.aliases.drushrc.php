0aba420f3ce276060f5e5bec0d01db54535d513b
Adam Dowling <adamd@deeson.co.uk>
2018-10-04 18:16:22 +0100

    Updated Docker and Drupal configuration to our latest best practice

--- drush/site.aliases.drushrc.php
+++ drush/site.aliases.drushrc.php
@@ -5,25 +5,10 @@
  * Drush environment aliases drush/sites.aliases.drushrc.php
  */
 
-// VDD.
-$aliases['vdd'] = array(
-  'env' => 'vdd',
-  'uri' => 'PROJECT.dev',
-  'root' => '/var/www/vhosts/PROJECT.dev/docroot',
-  'path-aliases' => array(
-    '%drush-script' => '/var/www/vhosts/PROJECT.dev/vendor/bin/drush',
-  ),
-);
-
-if (!file_exists('/var/www/vhosts/PROJECT.dev/docroot')) {
-  $aliases['vdd']['remote-host'] = 'dev.local';
-  $aliases['vdd']['remote-user'] = 'vagrant';
-}
-
 $aliases['docker'] = array(
   'env' => 'docker',
-  'uri' => 'localhost',
   'root' => '/var/www/html/docroot',
+  'uri' => getenv('D4D_HOSTNAME'),
   'path-aliases' => array(
     '%drush-script' => '/var/www/html/vendor/bin/drush',
   ),
